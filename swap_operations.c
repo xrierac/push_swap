4a635eab4ec39c9aca16356bc09c78f09f2ca436
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-12-04 15:20:17 +0200

    Add rorate and reverse_rotate functions

--- swap_operations.c
+++ swap_operations.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_operations.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:18:39 by xriera-c          #+#    #+#             */
+/*   Updated: 2023/12/04 15:18:26 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	*top_swap(int arr[])
 {
 	int	tmp;
@@ -5,7 +17,37 @@ int	*top_swap(int arr[])
 	tmp = arr[0];
 	arr[0] = arr[1];
 	arr[1] = tmp;
-	return (arr[]);
+	return (arr);
+}
+
+int	*rotate(int arr[], int len)
+{
+	int	tmp;
+	int	i;
+
+	i = 0;
+	tmp = arr[i];
+	while (i < len - 1)
+	{
+		arr[i] = arr[i + 1];
+		i++;
+	}
+	arr[i] = tmp;
+	return (arr);
+}
+
+int	*reverse_rotate(int arr[], int len)
+{
+	int	tmp;
+
+	tmp = arr[len - 1];
+	while ( len > 1)
+	{
+		arr[len - 1] = arr[len - 2];
+		len--;
+	}
+	arr[0] = tmp;
+	return (arr);
 }
 
 int	*top_push(int taker[], int giver[])

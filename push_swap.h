b142e1ffb1b91d1196e621357cd3ab377253abdf
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-01-08 17:38:49 +0200

    Add structwq

--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,19 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 11:28:05 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/01/08 11:56:17 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/01/08 14:23:42 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+typedef struct	s_value
+{
+	int	number;
+	int	index_up;
+	int	index_down;
+}	t_value;
 
 void	increase(int arr[], int *len);
 void 	decrease(int arr[], int *len);

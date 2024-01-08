5366a47728d88670b00d27629dcf4fbb52f3d6de
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-01-08 17:38:07 +0200

    Split function

--- insertion_sort.c
+++ insertion_sort.c
@@ -6,32 +6,66 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:11:57 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/01/08 11:49:10 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:36:52 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-void	insertion_sort(int arra[], int arrb[], int *lena, int *lenb)
+int	find_min_index(int arr[], int *len)
 {
 	int	i;
 	int	min;
 	int	index;
 
+	i = 0;
+	min = arr[0];
+	index = 0;
+	while (i < *len)
+	{
+		if (min > arr[i])
+		{
+			min = arr[i];
+			index = i;
+		}
+		i++;
+	}
+	return (index);
+}
+
+int	find_max_index(int arr[], int *len)
+{
+	int	i;
+	int	max;
+	int	index;
+
+	i = 0;
+	max = arr[0];
+	index = 0;
+	while (i < *len)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+			index = i;
+		}
+		i++;
+	}
+	return (index);
+}
+
+void	insertion_sort(int arra[], int arrb[], int *lena, int *lenb)
+{
+	int	min;
+	int	index;
+	int	size;
+
+	size = *lena;
 	while (*lena != 0)
 	{
-		i = 0;
-		min = arra[0];
-		while (i < *lena)
-		{
-			if (min > arra[i])
-			{
-				min = arra[i];
-				index = i;
-			}
-			i++;
-		}
+		index = find_min_index(arra, lena);
+		min = arra[index];
 		if (index < (*lena / 2))
 		{
 			while (arra[0] != min)

b00d46d269d63bd7f02de075ba9849e43f377b31
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-01-09 14:51:20 +0200

    Add function to create ranked array

---
+++ create_rank.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_rank.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 11:10:53 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/01/09 14:49:17 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	create_rank(int stack[], int sorted[], int *len)
+{
+	int	i;
+	int j;
+	int	count;
+
+	i = -1;
+	while (++i < *len)
+	{
+		count = 0;
+		j = -1;
+		while (++j < *len)
+		{
+			if (stack[i] > stack[j])
+					count++;
+		}
+		sorted[i] = count;
+	}
+}

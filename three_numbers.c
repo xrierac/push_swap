62c9acee5f0546cc83739a666e874f299f36ca92
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-22 17:10:49 +0200

    Correct function

--- three_numbers.c
+++ three_numbers.c
@@ -6,28 +6,28 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:32:10 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/21 14:01:34 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/22 17:07:51 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	three_numbers(int arra[], int len)
+void	three_numbers(t_stack *s)
 {
-	if (arra[0] > arra[1] && arra[0] < arra[2])
-		sa(arra);
-	else if (arra[0] > arra[1] && arra[1] > arra[2])
+	if (s->stack[0] > s->stack[1] && s->stack[0] < s->stack[2])
+		sa(s->stack);
+	else if (s->stack[0] > s->stack[1] && s->stack[1] > s->stack[2])
 	{
-		sa(arra);
-		rra(arra, len);
+		sa(s->stack);
+		rra(s->stack, &s->len);
 	}
-	else if (arra[0] > arra[1] && arra[1] < arra[2])
-		ra(arra, len);
-	else if (arra[0] < arra[1] && arra[1] > arra[2])
+	else if (s->stack[0] > s->stack[1] && s->stack[1] < s->stack[2])
+		ra(s->stack, &s->len);
+	else if (s->stack[0] < s->stack[1] && s->stack[0] < s->stack[2])
 	{
-		sa(arra);
-		ra(arra, len);
+		sa(s->stack);
+		ra(s->stack, &s->len);
 	}
-	else if (arra[0] < arra[1] && arra[1] > arra[2])
-		rra(arra, len);
+	else if (s->stack[0] < s->stack[1] && s->stack[1] > s->stack[2])
+		rra(s->stack, &s->len);
 }

05875a32a9fb2df6e6d6e252686040660a06271b
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-22 17:11:47 +0200

    Add function to file

--- push.c
+++ push.c
@@ -6,12 +6,25 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:27:55 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/01/18 15:08:56 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/22 16:10:42 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
+
+static void	top_push(int taker[], int giver[], int *len_taker, int *len_giver)
+{
+	int	tmp;
+
+	if (*len_giver != 0)
+	{
+		tmp = giver[0];
+		decrease(giver, len_giver);
+		increase(taker, len_taker);
+		taker[0] = tmp;
+	}
+}
 
 void	pa(int arra[], int arrb[], int *lena, int *lenb)
 {

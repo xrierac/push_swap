d8ca6f24643caf4c1aeccf71ab247ccfc49b18fe
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-22 17:17:05 +0200

    Remove static functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 11:28:05 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/21 14:58:23 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/22 16:09:55 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,32 @@
 #  define ARRAY_SIZE 1000
 # endif
 
-typedef struct	s_stack
+typedef struct s_stack
 {
 	int	len;
 	int	stack[ARRAY_SIZE];
 }	t_stack;
 
-typedef struct	s_cost
+typedef struct s_cost
 {
+	int	target_val;
 	int	value;
 	int	push_price;
 	int	target;
-	int	current_position;
+	int	position;
 }	t_cost;
 
 void	increase(int arr[], int *len);
-void 	decrease(int arr[], int *len);
-void	top_push(int taker[], int giver[], int *len_taker, int *len_giver);
+void	decrease(int arr[], int *len);
 void	rotate(int arr[], int *len);
 void	reverse_rotate(int arr[], int *len);
-void	swap(int arr[]);
-void	insertion_sort(int arra[], int arrb[], int *lena, int *lenb);
 void	create_rank(int stack[], int sorted[], int *len);
-void	three_numbers(int arr[], int *len);
+void	three_numbers(t_stack *stack);
 int		get_size(int argc, char *argv[]);
 int		parsing(int argc, char *argv[], int stack[]);
 int		check_error(t_stack stack);
-void	sorting(t_stack *sta, t_stack *stb);
+void	sort(t_stack *sta, t_stack *stb);
+int		find_min_index(int arr[], int *len);
 
 //Operations
 void	sa(int arr[]);

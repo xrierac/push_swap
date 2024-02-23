1fd966c3b928af16e37db9dddd4d5d8d87248257
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-23 13:39:57 +0200

    Modify arguments to structs

--- utils.c
+++ utils.c
@@ -6,32 +6,33 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 16:17:10 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/22 16:17:16 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/23 11:51:41 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	increase(int arr[], int *len)
+#include "push_swap.h"
+
+void	increase(t_stack *stack)
 {
 	int	i;
 
 	i = 0;
-	while (i < *len)
+	while (i < stack->len)
 	{
-		arr[*len - i - 1] = arr[*len - i - 2];
+		stack->stack[stack->len - i - 1] = stack->stack[stack->len - i - 2];
 		i++;
 	}
-	*len = *len + 1;
+	stack->len = stack->len + 1;
 }
-
-void	decrease(int arr[], int *len)
+void	decrease(t_stack *stack)
 {
 	int	i;
 
 	i = 0;
-	while (i < *len)
+	while (i < stack->len)
 	{
-		arr[i] = arr[i + 1];
+		stack->stack[i] = stack->stack[i + 1];
 		i++;
 	}
-	*len = *len - 1;
+	stack->len = stack->len - 1;
 }

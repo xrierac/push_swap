bbff6166e4a1960f79c6d7a545d1f5a6ffb14112
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-23 12:17:24 +0200

    Change arguments for t_stack

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -6,38 +6,38 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:39:06 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/22 16:04:20 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/23 12:08:12 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-void	reverse_rotate(int arr[], int *len)
+void	reverse_rotate(t_stack *st)
 {
 	int	tmp;
 
-	tmp = arr[*len - 1];
-	increase(arr, len);
-	*len = *len - 1;
-	arr[0] = tmp;
+	tmp = st->stack[st->len - 1];
+	increase(st);
+	st->len = st->len - 1;
+	st->stack[0] = tmp;
 }
 
-void	rra(int arr[], int *len)
+void	rra(t_stack *stack)
 {
-	reverse_rotate(arr, len);
+	reverse_rotate(stack);
 	ft_printf("rra\n");
 }
 
-void	rrb(int arr[], int *len)
+void	rrb(t_stack *stack)
 {
-	reverse_rotate(arr, len);
+	reverse_rotate(stack);
 	ft_printf("rrb\n");
 }
 
-void	rrr(int arra[], int arrb[], int *lena, int *lenb)
+void	rrr(t_stack *sta, t_stack *stb)
 {
-	reverse_rotate(arra, lena);
-	reverse_rotate(arrb, lenb);
+	reverse_rotate(sta);
+	reverse_rotate(stb);
 	ft_printf("rrr\n");
 }
--- rotate.c
+++ rotate.c
@@ -6,38 +6,38 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:33:35 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/22 16:05:29 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/23 12:14:12 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-void	rotate(int arr[], int *len)
+void	rotate(t_stack *stack)
 {
 	int	tmp;
 
-	tmp = arr[0];
-	decrease(arr, len);
-	*len = *len + 1;
-	arr[*len - 1] = tmp;
+	tmp = stack->stack[0];
+	decrease(stack);
+	stack->len = stack->len + 1;
+	stack->stack[stack->len - 1] = tmp;
 }
 
-void	ra(int arr[], int *len)
+void	ra(t_stack *stack)
 {
-	rotate(arr, len);
+	rotate(stack);
 	ft_printf("ra\n");
 }
 
-void	rb(int arr[], int *len)
+void	rb(t_stack *stack)
 {
-	rotate(arr, len);
+	rotate(stack);
 	ft_printf("rb\n");
 }
 
-void	rr(int arra[], int arrb[], int *lena, int *lenb)
+void	rr(t_stack *sta, t_stack *stb)
 {
-	rotate(arra, lena);
-	rotate(arrb, lenb);
+	rotate(sta);
+	rotate(stb);
 	ft_printf("rr\n");
 }

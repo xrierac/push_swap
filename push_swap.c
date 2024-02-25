f49cdf6bf0ccd6d70cc906b64fc23c34aa7fc9a9
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-25 14:15:52 +0200

    Add function to check if list is ordered and add condition to check if list is NULL

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,29 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 14:18:26 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/23 16:34:10 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/25 14:07:50 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
+
+static int	check_order(t_stack *sta)
+{
+	int	i;
+	int	prev;
+
+	i = -1;
+	prev = -1;
+	while (++i < sta->len)
+	{
+		if (sta->stack[i] > prev)
+			prev = sta->stack[i];
+		else
+			return (1);
+	}
+	return (0);
+}
 
 static int	print_error(void)
 {
@@ -28,13 +45,15 @@ int	main(int argc, char *argv[])
 	if (argc == 1)
 		return (0);
 	sta.len = get_size(argc, argv);
-	if (sta.len > ARRAY_SIZE)
+	if (sta.len > ARRAY_SIZE || sta.len <= 0)
 		return (print_error());
 	stb.len = 0;
 	rkd.len = sta.len;
 	if (parsing(argc, argv, sta.stack) == -1 || check_error(sta) == -1)
 		return (print_error());
 	create_rank(sta.stack, rkd.stack, &sta.len);
+	if (check_order(&rkd) == 0)
+		return (0);
 	sort(&rkd, &stb);
 	return (0);
 }

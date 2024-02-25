08761843bda14bfd7fadc0bdff38173fa7466b51
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-25 15:21:41 +0200

    Add function to check if there are no letters after the number and start function to check if number is bigger than MAXINT or smaller than MININT

--- parsing.c
+++ parsing.c
@@ -6,23 +6,49 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:08:17 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/25 14:16:14 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/25 15:20:06 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-static int	populate_array(char **arr, int stack[], int reset)
+static int	check_limit(char *str)
+{
+	if (ft_strlen(str) > 9)
+	{
+		if (*str == '-' || *str == '+')
+			return (0);
+	}
+	return (0);
+}
+
+static int	check_isnumber(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+static int	populate_array(char **arr, int stack[])
 {
 	static int	i = 0;
 	int			j;
 	char		*check;
 
-	if (reset == 1)
-		i = 0;
 	j = -1;
 	while (arr[++j] != '\0')
 	{
+		if (check_isnumber(arr[j]) == -1 || check_limit(arr[j]) == -1)
+			return (-1);
 		stack[i] = ft_atoi(arr[j]);
 		if (stack[i] == -1 || stack[i] == 0)
 		{
@@ -36,8 +62,7 @@ static int	populate_array(char **arr, int stack[], int reset)
 		}
 		i++;
 	}
-	ft_free_array(arr);
-	return (i);
+	return (0);
 }
 
 int	parsing(int argc, char *argv[], int stack[])
@@ -49,8 +74,12 @@ int	parsing(int argc, char *argv[], int stack[])
 	while (++i < argc)
 	{
 		arr = ft_split(argv[i], ' ');
-		if (populate_array(arr, stack, i) == -1)
+		if (populate_array(arr, stack) == -1)
+		{
+			ft_free_array(arr);
 			return (-1);
+		}
+		ft_free_array(arr);
 	}
 	return (0);
 }
